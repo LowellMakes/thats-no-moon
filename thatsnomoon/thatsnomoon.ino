1b17e640ea66f90aeb3f6043533a2defc81e4531
leftyfb <leftyfb@left-click.org>
2016-01-31 20:56:50 -0500

    limit switches added. Currently not getting proper readings from ping. Not working.

--- thatsnomoon/thatsnomoon.ino
+++ thatsnomoon/thatsnomoon.ino
@@ -3,23 +3,28 @@
 #define TRIGGER_PIN A0
 #define ECHO_PIN A1
 
-#define FAR_LIMIT 200
+#define CLOSE_PIN 4
+#define OPEN_PIN  5
+
+#define FAR_LIMIT 125
 
 #define SPEED 50
 
-#define MAX_DISTANCE      300
+#define MAX_DISTANCE      500
+#define BAD_READING 500
 
-#define OPEN_TIME (2 * 1200)
-#define CLOSE_TIME (2 * 1200)
+#define MAX_OPEN_CLOSE_TIME 30
 
 #include <Servo.h>
 #include <NewPing.h>
+#include <RunningAverage.h>
 
 // Forward declarations
 int ping_cm_BugFix();
 
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+RunningAverage distanceRA(10);
 Servo servo;
 
 void motor_forward(){
@@ -32,7 +37,6 @@ void motor_reverse(){
   servo.attach(10);
   servo.write(180);
   Serial.println("Reverse");
-  
 }
 
 void motor_stop(){
@@ -41,50 +45,58 @@ void motor_stop(){
   servo.detach();
 }
 
-void trigger() {
-  pinMode(ECHO_PIN, OUTPUT);
-  digitalWrite(ECHO_PIN, LOW);
-  pinMode(ECHO_PIN, INPUT);
-  digitalWrite(TRIGGER_PIN, LOW);
-  delayMicroseconds(4);
-  digitalWrite(TRIGGER_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIGGER_PIN, LOW);
-}
-
-int ball_open = false;
 void open_ball(){
-  if(!ball_open){
+  if(!ball_is_open()){
+    Serial.println("distInCentimeters");
     Serial.println("Opening");
     motor_forward();
-    delay(OPEN_TIME);
+    int timeout = MAX_OPEN_CLOSE_TIME;
+    while(!ball_is_open() && (timeout > 0)){
+      timeout--;
+      delay(100);
+    };
     motor_stop();
-    delay(2000);
-    ball_open = true;
+    if(ball_is_open()){
+      Serial.println("Opened");
+    }
   }
-    
 }
 
 void close_ball(){
-  //ball_open=false;
-  if(ball_open){
+  if(!ball_is_closed()){
     Serial.println("Closing");
     motor_reverse();
-    delay(CLOSE_TIME);
+    int timeout = MAX_OPEN_CLOSE_TIME;
+    while(!ball_is_closed() && (timeout > 0)){
+      timeout--;
+      delay(100);
+    };
     motor_stop();
-    delay(2000);
-    ball_open = false;
+    if(ball_is_closed()){
+      Serial.println("Closed");
+    }
   }
 }
 
+bool ball_is_closed(){
+  return !digitalRead(CLOSE_PIN);
+  Serial.println("top pin");
+}
+
+bool ball_is_open(){
+  return !digitalRead(OPEN_PIN);
+  Serial.println("bottom pin");
+}
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Begin");
   pinMode(ECHO_PIN, INPUT);
   pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(CLOSE_PIN, INPUT);
+  pinMode(OPEN_PIN, INPUT);
   servo.attach(10);
-//  servo.write(93);
+  distanceRA.clear(); //clear the running average
 }
 
 void loop() {
@@ -94,21 +106,41 @@ void loop() {
   int k;
 
   distInCentimeters = ping_cm_BugFix();
-
-    Serial.print("cm = ");
-    Serial.print(distInCentimeters);
-    Serial.println();
-    
-    if(distInCentimeters < 10){
-     delay(100);
+  if(distInCentimeters == BAD_READING){
+    if(ball_is_open()){
+      close_ball();
     }
-    else if(distInCentimeters < FAR_LIMIT){
-     open_ball();
-    }
-    else if(distInCentimeters > FAR_LIMIT){
-     close_ball();
-    }
+    return;
   }
+  
+  distanceRA.addValue(distInCentimeters);
+  distInCentimeters = distanceRA.getAverage();
+
+  Serial.print("cm = ");
+  Serial.print(distInCentimeters);
+  Serial.println();
+    
+
+  if(ball_is_closed()){
+    Serial.println("Ball is closed");
+  } 
+
+  if(ball_is_open()){
+    Serial.println("Ball is opened");
+  }
+
+  if(distInCentimeters < FAR_LIMIT){
+    open_ball();
+    Serial.println(distInCentimeters);
+    Serial.println(" ");
+  }
+//  else if((distInCentimeters > FAR_LIMIT) || (distInCentimeters == 0)){
+    else if(distInCentimeters > FAR_LIMIT){
+    Serial.println(distInCentimeters);
+    close_ball();
+    Serial.println(" ");
+  }
+}
 
 long microsecondsToCentimeters(long microseconds) {
   return microseconds / 29 / 2;
@@ -118,15 +150,15 @@ long microsecondsToCentimeters(long microseconds) {
 // HC-SR04 modules where they can get stuck return zero forever
 int ping_cm_BugFix(){
   
-        int distCm = sonar.ping_cm();
-        
-        if(distCm == 0){
-          delay(100);
-          pinMode(ECHO_PIN, OUTPUT);
-          digitalWrite(ECHO_PIN, LOW);
-          delay(100);
-          pinMode(ECHO_PIN, INPUT);
-        }
+  long echoTime = sonar.ping();
 
-        return distCm;
+  if(echoTime == 0){
+    delay(100);
+    pinMode(ECHO_PIN, OUTPUT);
+    digitalWrite(ECHO_PIN, LOW);
+    delay(100);
+    pinMode(ECHO_PIN, INPUT);
+    return BAD_READING;
+  }
+  return sonar.convert_cm(echoTime);
 }
